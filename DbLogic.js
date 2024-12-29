3d855af50518ac7b59ae90975f052577c1c0cda5
oshriv65 <oshriv65@gmail.com>
2024-12-29 16:22:23 +0200

    fix the issue

--- DbLogic.js
+++ DbLogic.js
@@ -1,9 +1,6 @@
-//const users = { admin: "admin" };
 function loadDB() {
-  if (!localStorage.length) {
-    localStorage.setItem("users", JSON.stringify({ admin: "admin" }));
-    localStorage.setItem("orders", JSON.stringify({}));
-  }
+  localStorage.setItem("users", JSON.stringify({ admin: "admin" }));
+  localStorage.setItem("orders", JSON.stringify({}));
 }
 function checkIfUserExists(username, password) {
   let users = JSON.parse(localStorage.getItem("users"));
@@ -15,16 +12,11 @@ function saveOrderToLocalMemory(key, value) {
   let orders = JSON.parse(localStorage.getItem("orders"));
   orders[key] = value;
   localStorage.setItem("orders", JSON.stringify(orders));
-  /*
-  localStorage.setItem(key, value);
-  */
 }
 
 // Clears all data from local memory
 function clearAllDataFromLocalMemory() {
-  let orders = JSON.parse(localStorage.getItem("orders"));
-  orders = {};
-  localStorage.setItem("orders", JSON.stringify(orders));
+  localStorage.setItem("orders", JSON.stringify({}));
 }
 
 // Retrieves all orders from local memory and concatenates them into a single string
@@ -34,29 +26,18 @@ function getAllOrdersFromLocalMemory() {
   for (const key in orders) {
     data += orders[key]; // Prints each value
   }
-  /*
-  for (let i = 0; i < localStorage.length; i++) {
-    data += localStorage.getItem(localStorage.key(i)); // Get the corresponding value
-  }
-  */
+
   return data || "אין הזמנות..."; // If no data, return a message indicating that there are no orders
 }
 
 // Checks if an order exists in local memory by key
 function checkIfOrderExistsInLocalMemory(key) {
   let orders = JSON.parse(localStorage.getItem("orders"));
-  console.log(key in orders);
   return key in orders;
-  /*
-  return localStorage.getItem(key) !== null;
-  */
 }
 
 // Retrieves an order from local memory by key
 function getOrderFromLocalMemory(key) {
   let orders = JSON.parse(localStorage.getItem("orders"));
   return orders[key];
-  /*
-  return localStorage.getItem(key);
-*/
 }
